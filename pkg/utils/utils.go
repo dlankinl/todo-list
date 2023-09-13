818644f4e533f22013706e555cef7dce232e7b99
Dmitry <dmit.lankin@mail.ru>
2023-09-13 22:10:14 +0300

    handlers and routes

---
+++ pkg/utils/utils.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"todo/services"
+)
+
+type Envelope map[string]interface{}
+
+func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(data)
+	if err != nil {
+		return err
+	}
+	err = decoder.Decode(&struct{}{}) // TODO: eliminate if possible
+	if err != io.EOF {
+		return errors.New("body must have only a single json value")
+	}
+	return nil
+}
+
+func ErrorJSON(w http.ResponseWriter, err error, status ...int) {
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+	var payload services.JSONResponse
+	payload.Error = true
+	payload.Msg = err.Error()
+	WriteJSON(w, statusCode, payload)
+}
